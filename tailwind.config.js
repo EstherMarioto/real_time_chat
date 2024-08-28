56b847e9445dec488f9dd7fcbddf46186c4fe597
Esther Marioto <mariotoesther08@gmail.com>
2024-08-28 01:21:47 -0300

    add font to text

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        openSans: ["Open Sans"],
+        roboto: ["Roboto"]
+      }
+    },
   },
   plugins: [],
 }
