23a202f17a20fcd74ea419df32358a97d87d12d4
Esther Marioto <mariotoesther08@gmail.com>
2024-08-28 03:14:00 -0300

    registration screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Login } from "./components/Auth/Login";
+import { Register } from "./components/Auth/Register";
 
 export function App() {
   return <Login />;
--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,11 +1,17 @@
-export function Login() {
+export function Register() {
   return (
     <div className="flex flex-col items-center mt-28">
       <h1 className="font-openSans font-black text-primary text-5xl mb-16">
         Real Time Chat
       </h1>
-      <h2 className="font-bold text-2xl mb-7">Faça Login</h2>
+      <h2 className="font-bold text-2xl mb-7">Inscreva-se</h2>
       <form action="" method="post" className="w-1/4">
+        <input
+          type="text"
+          name="username"
+          className="border w-full rounded-full border-black py-3 px-5 mb-5"
+          placeholder="Nome de usuário"
+        />
         <input
           type="email"
           name="email"
@@ -22,11 +28,11 @@ export function Login() {
           type="submit"
           className="bg-primary w-full rounded-full py-3 text-white font-semibold"
         >
-          Entrar
+          Inscreva-se
         </button>
       </form>
       <h6 className="mt-8 text-sm text-gray-400">
-        Não tem uma conta? <span className="text-primary">Inscrever-se</span>
+        Você tem uma conta? <span className="text-primary">Conecte-se</span>
       </h6>
     </div>
   );
